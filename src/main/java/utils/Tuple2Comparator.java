c98cbfce435a6578e9521bdcd8ddb7e56fe041c5
martina97 <54946553+martina97@users.noreply.github.com>
2022-06-14 03:20:47 +0200

    Update Tuple2Comparator.java

--- src/main/java/utils/Tuple2Comparator.java
+++ src/main/java/utils/Tuple2Comparator.java
@@ -10,9 +10,7 @@ public class Tuple2Comparator implements Comparator<Tuple2<String,Integer>>, Ser
     private static final long serialVersionUID = 1L;
     @Override
     public int compare(Tuple2<String, Integer> v1, Tuple2<String, Integer> v2) {
-        //System.out.println("v1._1() : " + v1._1() + ", v2._1() : " + v2._1());
-        //System.out.println("v1._1().compareTo(v2._1() = " + v1._1().compareTo(v2._1()));
-
+      
         if (v1._1().compareTo(v2._1()) == 0) {
             return v2._2().compareTo(v1._2());
         }

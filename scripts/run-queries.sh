a4fc9475249bba12527cbdee6bdbeba769de7ce8
martina97 <54946553+martina97@users.noreply.github.com>
2022-06-14 03:14:44 +0200

    Delete scripts directory

--- scripts/run-queries.sh
+++
@@ -1,2 +0,0 @@
-sudo docker exec spark /bin/bash -c "spark-submit --class utilities.CsvWriter --master \"local\" /utilities/sabd_project1-1.0-SNAPSHOT.jar"
-#sudo docker exec spark /bin/bash -c "spark-submit --class queries.StartQueries --master \"local\" /queries/sabd_project1-1.0-SNAPSHOT.jar"
--- scripts/start-dockers.sh
+++
@@ -1,1 +0,0 @@
-sudo docker-compose up --scale spark-worker=3 --scale hdfs-datanode=5 -d
